6e7ff11290ad16e025de36523e9b18e93b10eb98
Gabriel Watkinson <gabriel.watkinson@ensae.fr>
2021-04-10 07:10:21 +0200

    Update css style

--- src/dash/home/home.py
+++ src/dash/home/home.py
@@ -1,10 +1,17 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from src.dash.app import app
 
 layout = html.Div(
     children= \
-        [html.H1(children="Too Good To Go Analytics", className="header-title"),
+        [html.Img(
+            src=app.get_asset_url('favicon.ico'),
+            height='40 px',
+            width='auto',
+            className='header-image'
+        ),
+         html.H1(children="Too Good To Go Analytics", className="header-title"),
          html.P(
             children="Bienvenue sur Too Good To Go Analytics ! "
                      "Commencez par choisir un onglet à visiter :",
--- src/dash/model_report/model_report.py
+++ src/dash/model_report/model_report.py
@@ -5,7 +5,13 @@ from src.dash.app import app
 
 layout = html.Div(
     children= \
-        [html.H1(children="Too Good To Go Analytics", className="header-title"),
+        [html.Img(
+            src=app.get_asset_url('favicon.ico'),
+            height='40 px',
+            width='auto',
+            className='header-image'
+        ),
+         html.H1(children="Too Good To Go Analytics", className="header-title"),
          html.P(
              children="Bienvenue sur Too Good To Go Analytics ! "
                       "Commencez par choisir un onglet à visiter :",
